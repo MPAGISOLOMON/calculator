082e59af7671f8c80002b2946a13bcabf7883a36
unknown <kimerampagisolo30@gmail.com>
2015-10-21 16:09:06 -0700

    calculator

---
+++ Calc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class 1Calc {
+	
+public static void main(String args[]){
+Scanner Calc = new Scanner(System.in) ;
+double One, Two, Type, End;
+System.out.printin("Enter frist number:");
+One = calc.nextDouble();
+}
+}
